bec585441c8ef5289747afd9a73d6d7d96a53108
660853 <jj2497@comcast.net>
2020-10-30 14:51:15 +0000

    finished assignment

--- index.java
+++ index.java
@@ -6,13 +6,24 @@ void draw() {
 	background(255);
 	stroke(0);
 
-  //start creating the lines
-
-
-  //Fill a black color
+  line(200,0,200,400);
+  line(0,200,400,200);
   
+  fill(0);
+  
+  if (mouseX<200 && mouseY<200) {
+    rect(0,0,200,200);
+  }
 
-  //Build your conditional statement 
-	
+  if (mouseX>200 && mouseY<200) {
+    rect(200,0,200,200);
+  }
 
+  if (mouseX<200 && mouseY>200) {
+    rect(0,200,200,200);
+  }
+
+  if(mouseX>200 && mouseY>200) {
+    rect(200,200,200,200);
+  }
 }
