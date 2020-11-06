282c8c370f044a7a91d8316fddffe041229f4ab6
660853 <jj2497@comcast.net>
2020-11-06 15:24:05 +0000

    rollover with comments

--- index.java
+++ index.java
@@ -5,24 +5,24 @@ void setup() {
 void draw() {
 	background(255);
 	stroke(0);
-
+  //divding lines
   line(200,0,200,400);
   line(0,200,400,200);
   
   fill(0);
-  
+  //top left
   if (mouseX<200 && mouseY<200) {
     rect(0,0,200,200);
   }
-
+  //top right
   if (mouseX>200 && mouseY<200) {
     rect(200,0,200,200);
   }
-
+  //bottom left
   if (mouseX<200 && mouseY>200) {
     rect(0,200,200,200);
   }
-
+  //bottom right
   if(mouseX>200 && mouseY>200) {
     rect(200,200,200,200);
   }
